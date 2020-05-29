2b582d8eb28b243eb8d05518a163fb2f02ad82d9
morris-ribs <maumribeiro@gmail.com>
2020-05-29 17:22:10 +0000

    Modifications to DAG

--- dags/store_DAG.py
+++ dags/store_DAG.py
@@ -5,7 +5,7 @@ from airflow.operators.bash_operator import BashOperator
 
 default_args = {
     "owner": "Airflow",
-    "start_date": datetime(2020, 5, 29),
+    "start_date": datetime(2020, 5, 28),
     "retries": 1,
     "retry_delay": timedelta(seconds=5), # every failed task will be retried within 5 seconds
 }
@@ -14,6 +14,5 @@ dag = DAG("store_dag",
     default_args=default_args, schedule_interval='@daily',catchup=False) # catchup false turns off backfilling
 
 # first task is to check whether the file exists or not
-t1 = BashOperator(task_id='check_file_exists', bash_command='shasum ~/store_files_airflow/raw_store_transactions.,csv', retry_delay=timedelta(seconds=15, dag=dag)
-
+t1 = BashOperator(task_id='check_file_exists', bash_command='shasum ~/store_files_airflow/raw_store_transactions.csv', retry_delay=timedelta(seconds=15), dag=dag)
 
